9bb30da2d55df80dbd0ab9949d91a913dcb6a8a4
Otavio Pereira (Ramal 27) <otavio@XYZTEMAS.local>
2016-04-19 17:03:15 -0300

    Accepted

---
+++ URI_1018.cs
@@ -0,0 +1,27 @@
+/*
+  Status Submissão: Accepted!
+
+  Leia um valor inteiro. A seguir, calcule o menor número de notas possíveis
+  (cédulas) no qual o valor pode ser decomposto. As notas consideradas são de
+  100, 50, 20, 10, 5, 2 e 1. A seguir mostre o valor lido e a relação de notas
+  necessárias.
+*/
+using System;
+
+class URI{
+  static void Main(string[] args){
+    int[] x = {100, 50, 20, 10, 5, 2, 1};
+    int aux;
+
+    int y = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("{0}", y);
+    for (int i = 0; x.Length > i; ++i )
+    {
+        aux = y/x[i];
+        Console.WriteLine("{0} nota(s) de R$ {1},00", aux, x[i]);
+        aux = (y%x[i]);
+        y = aux;
+    }
+  }
+}
