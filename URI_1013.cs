8c5d7b6212ba4b17a8887479e3942ec027dc191e
Otavio Pereira (Ramal 27) <otavio@XYZTEMAS.local>
2016-04-25 17:42:35 -0300

    Accepted

--- URI_1013.cs
+++ URI_1013.cs
@@ -1,5 +1,5 @@
 /*
-  Status Submissão: Wrong Answer ⚠
+  Status Submissão: Accepted ☑
 
   Faça um programa que leia três valores e apresente o maior dos três valores
   lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
@@ -9,11 +9,13 @@ using System;
 
 class URI{
   static void Main(string[] args){
-    int a = int.Parse(Console.ReadLine());
-    int b = int.Parse(Console.ReadLine());
-    int c = int.Parse(Console.ReadLine());
+    string[] y = Console.ReadLine().Split();
+    int a = int.Parse(y[0]);
+    int b = int.Parse(y[1]);
+    int c = int.Parse(y[2]);
     int d = ((a+b)+System.Math.Abs(a-b))/2;
+    int e = ((c+d)+System.Math.Abs(c-d))/2;
 
-    Console.WriteLine("{0} eh maior", ((c+d)+System.Math.Abs(c-d))/2);
+    Console.WriteLine("{0} eh o maior", e);
   }
 }
