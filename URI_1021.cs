121f454571eeb2df501aa1784bfdb0a298299dab
Otavio Pereira <otajesus@bol.com.br>
2016-05-02 13:46:33 -0300

    Accepted

--- URI_1021.cs
+++ URI_1021.cs
@@ -1,5 +1,5 @@
 /*
-  Status Submissão:
+  Status Submissão: Accepted ☑
 
   Leia um valor de ponto flutuante com duas casas decimais. Este valor representa
   um valor monetário. A seguir, calcule o menor número de notas e moedas possíveis
@@ -15,32 +15,22 @@ class URI{
     double[] moedas = {1, 0.50, 0.25, 0.10, 0.05, 0.01};
     double aux;
 
-    string[] tipo_troca = {"nota", "moeda"};
-
     double valor_troca = double.Parse(Console.ReadLine());
 
-    for(int i = 0; tipo_troca.Length > i; ++i)
+    Console.WriteLine("NOTAS:");
+    for(int j = 0; notas.Length > j ; ++j)
     {
-      Console.WriteLine("{0}S:", tipo_troca[i].ToUpper());
-      if(tipo_troca[i].Equals("nota"))
-      {
-        for(int j = 0; notas.Length > j; ++j)
-        {
-          aux = valor_troca/notas[j];
-          Console.WriteLine("{0} {1}(s) de R$ {2}", Math.Floor(aux).ToString("0"), tipo_troca[i], notas[j].ToString("0.00"));
-          aux = (valor_troca%notas[j]);
-          valor_troca = aux;
-        }
-      } else
-      {
-        for(int j = 0; notas.Length > j; ++j)
-        {
-          aux = valor_troca/moedas[j];
-          Console.WriteLine("{0} {1}(s) de R$ {2} ", Math.Floor(aux).ToString("0"), tipo_troca[i], moedas[j].ToString("0.00"));
-          aux = (valor_troca%moedas[j]);
-          valor_troca = aux;
-        }
-      }
+      aux = valor_troca/notas[j];
+      Console.WriteLine("{0} nota(s) de R$ {1}", Math.Floor(aux).ToString("0"), notas[j].ToString("0.00"));
+      valor_troca = double.Parse((valor_troca%notas[j]).ToString("0.00"));
+    }
+
+    Console.WriteLine("MOEDAS:");
+    for(int j = 0; notas.Length > j; ++j)
+    {
+      aux = valor_troca/moedas[j];
+      Console.WriteLine("{0} moeda(s) de R$ {1}", Math.Floor(aux).ToString("0"), moedas[j].ToString("0.00"));
+      valor_troca = double.Parse((valor_troca%moedas[j]).ToString("0.00"));
     }
   }
 }
